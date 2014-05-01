8604697c397f986aca7e54622eac28a9dbb540a0
Cody Rioux <cody.rioux@gmail.com>
2014-05-01 12:35:47 -0600

    Clustering algorithm now always returns the requested amount of clusters, even if empty.

--- src/kmeans_clj/core.clj
+++ src/kmeans_clj/core.clj
@@ -2,9 +2,10 @@
   "A simple k-means clustering implementation."
   (:require [incanter.core :as i]
             [kmeans-clj.util :refer :all]
-            [clojure.core.async :refer [chan to-chan <!!]]))
+            [clojure.core.async :refer [chan to-chan <!!]]
+            [clojure.set :refer [union difference]]))
 
-(defn cluster-vector
+(defn- cluster-vector
   [fe cluster]
   (i/div (reduce i/plus (map fe cluster)) (count cluster)))
 
@@ -12,36 +13,37 @@
   "Performs k-means clustering on the elements into k clusters until
    convergence or the maximum number of iterations is reached. Function
    distance is a pairwise distance function, and fe a feature extractor
-   that returns a vector for a given element.
-   
-   Note: This function currently does some group-by and seq manipulation
-   to form new clusters. This may however be a prime candidate for STM
-   and a ref."
+   that returns a vector for a given element."
   [elements distance fe k max-iter]
   (loop
     [clusters-1 nil
-     clusters (partition-all (quot (count elements) k) (shuffle elements))
+     clusters (map (partial cluster-vector fe) (partition-all k (shuffle elements)))
+     cluster-vals []
      iter 0]
     (if
-      (or (= clusters clusters-1) (= max-iter iter))
-      (concat (seq clusters) (repeat (- k (count clusters)) []))
+      (or (= clusters clusters-1) (= max-iter iter)) cluster-vals
       (let [input (to-chan elements)
             output (chan)
             results (sink output)
             _ (<!! (parallel
                      (.availableProcessors (Runtime/getRuntime))
-                     (fn [e] [(argmin
-                                #(distance (fe e)
-                                           (cluster-vector fe %))
-                                clusters)
-                              e])
+                     (fn [e]
+                       (let [evec (fe e)]
+                         [(argmin
+                            #(distance evec %)
+                            clusters)
+                          e]))
                      input
-                     output))]
+                     output))
+            new-cluster-vals (set (->>
+                                    @results
+                                    (group-by first)
+                                    (fmap #(map second %))
+                                    vals))]
         (recur
           clusters
-          (set (->>
-                 @results
-                 (group-by first)
-                 (fmap #(map second %))
-                 vals))
+          ; Ensure we do not lose any old clusters which did not receive a match
+          (union (set (map (partial cluster-vector fe) new-cluster-vals))
+                 (difference (set clusters) (set (map first @results))))
+          new-cluster-vals
           (inc iter))))))

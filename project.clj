0ae7339827d8b6df82bb85d78423ec5c59a0141d
Adam Tornhill <adam@adamtornhill.com>
2015-08-14 09:52:14 +0200

    Updated the dependencies to the latest library versions

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [incanter/incanter-core "1.5.0-SNAPSHOT"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [incanter/incanter-core "1.5.6"]])
